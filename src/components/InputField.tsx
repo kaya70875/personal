c07a258d78ba99f3eecea6f88464ca0a2a6d1170
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-31 22:17:02 +0300

    add placeholder to InputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 interface InputFieldProps {
     label?: string;
+    placeholder?: string;
     className?: string;
 }
 
-export default function InputField({ label = '', className }: InputFieldProps) {
+export default function InputField({ label = '', className, placeholder }: InputFieldProps) {
     return (
         <div className='flex flex-col gap-2 w-full'>
             {label && <label htmlFor={label}>{label}</label>}
-            <input type="text" className={`py-1 px-3 border border-text rounded-lg ${className}`} id={label} />
+            <input type="text" className={`py-1 px-3 border border-text rounded-lg ${className}`} placeholder={placeholder} id={label} />
         </div>
     )
 }
