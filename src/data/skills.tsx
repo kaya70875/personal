adbc9ee4b31bc2de7df54fceca351f67cf7a3c82
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-31 15:19:20 +0300

    create skills object for mapping

---
+++ src/data/skills.tsx
@@ -0,0 +1,60 @@
+import reactIconSVG from "../../public/svg/react.svg";
+import typescriptIconSVG from "../../public/svg/ts.svg";
+import manifestIconSVG from "../../public/svg/extension.svg";
+import pythonIconSVG from "../../public/svg/python.svg";
+import nextIconSVG from "../../public/svg/next.svg";
+import scssIconSVG from "../../public/svg/scss.svg";
+import tailwindIconSVG from "../../public/svg/tailwind.svg";
+import mongoIconSVG from "../../public/svg/mongo.svg";
+import firebaseIconSVG from "../../public/svg/firebase.svg";
+import fastapibaseIconSVG from "../../public/svg/fastapi-1.svg";
+import figmaIconSvg from "../../public/svg/figma.svg";
+
+export const SKILLS = [
+    {
+        name: 'React',
+        icon: reactIconSVG
+    },
+    {
+        name: 'Typescript',
+        icon: typescriptIconSVG
+    },
+    {
+        name: 'Next JS',
+        icon: nextIconSVG
+    },
+    {
+        name: 'Python',
+        icon: pythonIconSVG
+    },
+    {
+        name: 'MongoDB',
+        icon: mongoIconSVG
+    },
+    {
+        name: 'Fast API',
+        icon: fastapibaseIconSVG
+    },
+    {
+        name: 'Manifest',
+        icon: manifestIconSVG
+    },
+    {
+        name: 'Tailwind',
+        icon: tailwindIconSVG
+    },
+    {
+        name: 'SCSS',
+        icon: scssIconSVG
+    },
+    {
+        name: 'Firebase',
+        icon: firebaseIconSVG
+    },
+    {
+        name: 'Figma',
+        icon: figmaIconSvg
+    }
+
+
+] as const;
