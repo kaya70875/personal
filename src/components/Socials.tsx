8e489d7c4acfa99b3f583b686929daf3da49f8b2
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-31 21:19:19 +0300

    Create Socials component for reausability

---
+++ src/components/Socials.tsx
@@ -0,0 +1,21 @@
+import IconGithub from '@/svg/IconGithub'
+import IconIn from '@/svg/IconIn'
+import Link from 'next/link'
+import React from 'react'
+
+interface SocialsProps {
+    className?: string;
+}
+
+export default function Socials({ className = '' }: SocialsProps) {
+    return (
+        <div className={`socials text-text flex items-center gap-8 justify-center ${className}`}>
+            <Link href={'https://github.com/kaya70875'} target="__blank" className="transition-all duration-200 ease-in hover:text-accent">
+                <IconGithub />
+            </Link>
+            <Link href={'https://www.linkedin.com/in/ahmet-kaya-44a2172aa/'} target="__blank" className="transition-all duration-200 ease-in hover:text-accent">
+                <IconIn />
+            </Link>
+        </div>
+    )
+}
