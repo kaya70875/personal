212ef5fe74aa6be3b321e53a7cdcab966335416c
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-04 16:02:58 +0300

    add onChange to inputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,13 +4,14 @@ interface InputFieldProps {
     label?: string;
     placeholder?: string;
     className?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function InputField({ label = '', className, placeholder }: InputFieldProps) {
+export default function InputField({ label = '', className, placeholder, onChange }: InputFieldProps) {
     return (
         <div className='flex flex-col gap-2 w-full'>
             {label && <label htmlFor={label}>{label}</label>}
-            <input type="text" className={`py-1 px-3 border border-text rounded-lg ${className}`} placeholder={placeholder} id={label} />
+            <input type="text" onChange={onChange} className={`py-1 px-3 border border-text rounded-lg ${className}`} placeholder={placeholder} id={label} />
         </div>
     )
 }
