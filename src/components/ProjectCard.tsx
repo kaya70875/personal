e0a9521f03b31ea5055cdf10c3e83fa33249dee8
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-09 12:59:04 +0300

    implement view full size image with a modal component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,6 +4,7 @@ import Image, { StaticImageData } from 'next/image';
 import React from 'react'
 import Button from './Button';
 import Link from 'next/link';
+import { useModal } from '@/context/ImageModalContext';
 
 interface Link {
     live: string;
@@ -23,13 +24,19 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ name, image, desc, link }: ProjectCardProps) {
 
+    const { modalImage, setModalImage } = useModal();
+
     const projectButtonStyles = "min-w-32"
+
+    const handleImageClick = () => {
+        setModalImage({ ...modalImage, open: true, image: image.src })
+    }
 
     return (
         <div className="flex flex-col justify-between h-full items-center gap-8">
             <div className='flex flex-col gap-8 items-center'>
                 <header className='font-bold text-2xl text-text dark:text-dark-text uppercase'>{name}</header>
-                <Image className='rounded-lg' src={image.src} width={image.width ?? 500} height={image.height ?? 500} alt='project' />
+                <Image onClick={handleImageClick} className='rounded-lg cursor-pointer' src={image.src} width={image.width ?? 500} height={image.height ?? 500} alt='project' />
                 <p className='text-text dark:text-dark-text opacity-90 font-light'>{desc}</p>
             </div>
 
@@ -41,6 +48,8 @@ export default function ProjectCard({ name, image, desc, link }: ProjectCardProp
                     <Button className={projectButtonStyles} variant="secondary">Github</Button>
                 </Link>
             </div>
+
+
         </div>
 
     )
---
+++ src/components/modals/ImageModal.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useModal } from '@/context/ImageModalContext'
+import Image from 'next/image';
+import React from 'react'
+import { MdClose } from 'react-icons/md';
+import { motion } from 'motion/react';
+
+export default function ImageModal() {
+
+    const { modalImage, setModalImage } = useModal();
+
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2 }}
+            exit={{ opacity: 0, scale: 0 }}
+            className='fixed z-20 w-[75%] top-1/2 left-1/2 p-8 mt-8 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-dark-primary flex flex-col gap-4 rounded-lg'>
+            <div className='w-full flex items-center justify-end' onClick={() => setModalImage({ ...modalImage, open: false })}>
+                <MdClose className='cursor-pointer text-text dark:text-dark-text' />
+            </div>
+            <Image src={modalImage.image || ''} alt='modalImage' className='shadow-lg' />
+        </motion.div>
+    )
+}
