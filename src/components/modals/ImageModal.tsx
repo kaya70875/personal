57bfd56222503fd7cd42c4dfd9869c9756e0b6c9
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-09 14:40:51 +0300

    update: modalImage for mobile sizes

--- src/components/modals/ImageModal.tsx
+++ src/components/modals/ImageModal.tsx
@@ -16,11 +16,11 @@ export default function ImageModal() {
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.2 }}
             exit={{ opacity: 0, scale: 0 }}
-            className='fixed z-20 w-[75%] top-1/2 left-1/2 p-8 mt-8 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-dark-primary flex flex-col gap-4 rounded-lg'>
+            className='fixed z-20 w-full md:w-[75%] top-1/2 left-1/2 p-2 md:p-8 mt-8 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-dark-primary flex flex-col gap-4 rounded-lg'>
             <div className='w-full flex items-center justify-end' onClick={() => setModalImage({ ...modalImage, open: false })}>
                 <MdClose className='cursor-pointer text-text dark:text-dark-text' />
             </div>
-            <Image src={modalImage.image || ''} alt='modalImage' className='shadow-lg' />
+            <Image src={modalImage.image || ''} alt='modalImage' className='shadow-lg rounded-lg' />
         </motion.div>
     )
 }
