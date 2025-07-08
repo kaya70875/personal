f7a036371e16088849b3939495dc1792505b2567
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-08 21:33:39 +0300

    feat: inster rehype plugin for code blocks

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,5 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrism from "rehype-prism-plus";
 
 export const Post = defineDocumentType(() => ({
     name: 'Post',
@@ -16,4 +17,4 @@ export const Post = defineDocumentType(() => ({
     },
 }))
 
-export default makeSource({contentDirPath: 'posts', documentTypes: [Post], mdx: {}});
+export default makeSource({contentDirPath: 'posts', documentTypes: [Post], mdx: {rehypePlugins: [rehypePrism]}});
