9e99fccae9f1f2640990e8667149d9e183571246
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-08 14:52:09 +0300

    update blogCard component layout

--- src/app/blogs/components/BlogCard.tsx
+++ src/app/blogs/components/BlogCard.tsx
@@ -10,19 +10,21 @@ interface BlogCardProps {
 
 export default function BlogCard({ blog }: BlogCardProps) {
     return (
-        <div className="posts flex flex-col gap-4 p-4 w-full border border-secondary rounded-lg">
-            <h2>
-                <Link href={blog.url}>
+        <Link href={blog.url} className="posts flex flex-col w-full bg-white shadow-lg rounded-lg cursor-pointer border border-gray-200">
+            <div className='flex flex-col gap-4 h-60 relative'>
+                <Image fill className='rounded-t-lg border-b border-b-gray-200 object-cover' priority src={blog.thumbnail as string} alt={blog.title} />
+            </div>
+            <div className='flex flex-col gap-4 p-6'>
+                <h2 className='text-2xl text-text font-bold'>
                     {blog.title}
-                </Link>
-            </h2>
-            <Image src={blog.thumbnail as string} width={600} height={600} alt={blog.title} />
-            <time dateTime={blog.date}>
-                {format(parseISO(blog.date), 'LLLL d, yyyy')}
-            </time>
-            <p className="description break-words">
-                {blog.description}
-            </p>
-        </div>
+                </h2>
+                <time className='opacity-80' dateTime={blog.date}>
+                    {format(parseISO(blog.date), 'LLLL d, yyyy')}
+                </time>
+                <p className="description break-words">
+                    {blog.description}
+                </p>
+            </div>
+        </Link>
     )
 }
