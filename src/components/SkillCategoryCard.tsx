6027cd77d5b774b694438d08af61d9a9da216072
Ahmet Kaya <shortsforturkey@gmail.com>
2025-07-21 14:41:35 +0300

    refactor: update skillCard layout

--- src/components/SkillCategoryCard.tsx
+++ src/components/SkillCategoryCard.tsx
@@ -11,8 +11,9 @@ export default function SkillCategoryCard({ category }: SkillCategoryCard) {
   const currentCategory = SKILLS.filter((skill) => skill.category === category)
 
   return (
-    <div className='bg-white shadow-md dark:bg-dark-primary border h-full border-gray-300 dark:border-dark-bg p-6 flex flex-col gap-4 rounded-lg w-full max-w-lg'>
-      <h2 className='capitalize w-full text-center'>{category}</h2>
+    <div className='bg-white shadow-md dark:bg-dark-primary border h-full border-gray-300 dark:border-gray-500 p-6 flex flex-col gap-6 rounded-lg w-full max-w-lg'>
+      <h2 className='capitalize font-bold text-xl!'>{category}</h2>
+      <div className="line bg-gray-400" />
       <div className="skill-buttons grid grid-cols-2 gap-4">
         {currentCategory.map((skill, index) => (
           <SkillButton name={skill.name} icon={skill.icon} key={index} />
