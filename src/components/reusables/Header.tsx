b150699db435d00192ef974e4b5f6134163aa866
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-10 20:08:24 +0300

    refactor: header compoennt to accept description

--- src/components/reusables/Header.tsx
+++ src/components/reusables/Header.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 interface HeaderProps {
-    name: string
+    name: string;
+    desc?: string;
 }
 
-export default function Header({ name }: HeaderProps) {
+export default function Header({ name, desc }: HeaderProps) {
     return (
         <div className='flex flex-col w-full items-center mb-16'>
             <header className='section-header text-center'>
                 {name}
             </header>
-            <div className="header-line w-32 h-12 bg-accent"></div>
+            <div className="header-line w-32 h-12 bg-accent mb-4"></div>
+            {desc && (
+                <p className="desc font-light max-w-2xl text-center w-full opacity-90">{desc}</p>
+            )}
         </div>
     )
 }
