7df52dfb1c692a4c91935c9462f8ec764bd2ccb4
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-10 02:26:31 +0000

    decrease gap in Header component

--- src/components/reusables/Header.tsx
+++ src/components/reusables/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export default function Header({ name }: HeaderProps) {
     return (
-        <div className='flex flex-col gap-2 w-full items-center mb-24'>
+        <div className='flex flex-col w-full items-center mb-24'>
             <header className='section-header text-center'>
                 {name}
             </header>
