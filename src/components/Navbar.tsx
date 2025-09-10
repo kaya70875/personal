577f2ca1ae01aade910046992c699944f8144c1c
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-10 02:18:48 +0000

    improve navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,14 +40,14 @@ export default function Navbar() {
     }
 
     return (
-        <nav className='flex w-full items-center justify-between fixed top-0 left-0 px-[20px] md:px-[80px] xl:px-[150px] 2xl:px-[250px] py-4 md:py-6 shadow-xl z-30 backdrop-blur-sm'>
+        <nav className='flex w-full items-center justify-between fixed top-0 left-0 px-[20px] md:px-[80px] xl:px-[150px] 2xl:px-[250px] py-4 md:py-6 z-30 bg-bg dark:bg-dark-bg border border-gray-200 dark:border-gray-800'>
             <header>
                 <Link href={'/'}>
-                    <h2 className='text-text dark:text-dark-text'>ahmetky.Dev</h2>
+                    <h2 className='text-text dark:text-dark-text'><span className='text-accent font-bold'>ahmetky</span>.dev</h2>
                 </Link>
             </header>
 
-            <ul className='nav-items hidden md:flex items-center gap-10 text-text dark:text-dark-text cursor-pointer'>
+            <ul className='nav-items hidden md:flex items-center gap-8 text-text dark:text-dark-text cursor-pointer font-medium'>
                 {navLinks?.map((link, index) => (
                     <li key={index}>
                         <NavLink onClick={(e) => handleClick(e!, link.name)} href={link.url}>{link.name == 'Resume' ? (<div className='flex items-center gap-2'>{link.name} <Download className='w-4 h-4' /></div>) : link.name}</NavLink>
