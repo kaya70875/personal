cedaa40fa907ceed09bf36226f3c90bf263db34b
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-11 03:29:22 +0000

    update socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin } from 'lucide-react';
+import { Github, Linkedin, MailIcon } from 'lucide-react';
 import Link from 'next/link'
 import React from 'react'
 
@@ -8,13 +8,19 @@ interface SocialsProps {
 }
 
 export default function Socials({ className = '', gap = 'gap-8' }: SocialsProps) {
+
+    const linkStyle = 'transition-all duration-200 ease-in hover:text-accent transition-all duration-200 ease-in hover:text-accent';
+
     return (
         <div className={`socials text-text dark:text-dark-text flex items-center justify-center ${className} ${gap}`}>
-            <Link href={'https://github.com/kaya70875'} target="__blank" className="transition-all duration-200 ease-in hover:text-accent">
+            <Link href={'https://github.com/kaya70875'} target="__blank" className={linkStyle}>
                 <Github />
             </Link>
-            <Link href={'https://www.linkedin.com/in/ahmet-kaya-44a2172aa/'} target="__blank" className="transition-all duration-200 ease-in hover:text-accent">
+            <Link href={'https://www.linkedin.com/in/ahmet-kaya-44a2172aa/'} target="__blank" className={linkStyle}>
                 <Linkedin />
+            </Link>
+            <Link href={'mailto:kaya70875@gmail.com'} target='__blank' rel='noopener noreferrer' className={linkStyle}>
+                <MailIcon />
             </Link>
         </div>
     )
