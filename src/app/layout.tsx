5c59162abee21aecd6dffb91ace4e332491c8024
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-12 02:03:40 +0000

    remove light theme support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,13 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import { Poppins } from "next/font/google";
 import Footer from "@/components/Footer";
-import ThemeProvider from "@/context/ThemeContext";
 import ImageModalProvider from "@/context/ImageModalContext";
 import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "200", "300", "500", "600"]
-})
+  weight: ["400", "200", "300", "500", "600"],
+});
 
 export const metadata: Metadata = {
   title: "ahmetky.dev",
@@ -24,35 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function() {
-              const theme = localStorage.getItem('theme');
-              if (theme === 'dark') {
-                document.documentElement.setAttribute('data-theme', 'dark');
-              } else {
-                document.documentElement.setAttribute('data-theme', 'light');
-              }
-            })();
-          `,
-          }}
-        />
-      </head>
-      <body
-        className={`${poppins.className} antialiased`}
-      >
-        <ThemeProvider>
-          <ImageModalProvider>
-            <div className="bg-bg dark:bg-dark-bg flex flex-col sections-gap min-h-screen relative">
-              <Navbar />
-              {children}
-              <Analytics />
-            </div>
-            <Footer />
-          </ImageModalProvider>
-        </ThemeProvider>
+      <body className={`${poppins.className} antialiased`}>
+        <ImageModalProvider>
+          <div className="bg-bg dark:bg-dark-bg flex flex-col sections-gap min-h-screen relative">
+            <Navbar />
+            {children}
+            <Analytics />
+          </div>
+          <Footer />
+        </ImageModalProvider>
       </body>
     </html>
   );
